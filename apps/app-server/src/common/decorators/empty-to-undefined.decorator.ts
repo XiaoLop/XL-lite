7c514044f2699e9f7bd0d2f30999446f355f13fc
Lu <18398898924@163.com>
2026-06-23 21:02:35 +0800

    ``` feat(common): 空值转换装饰器支持布尔值转换
    空值转换装饰器现在不仅将空字符串、null和undefined转换为undefined，
    还增加了对字符串'true'和'false'的处理，将其分别转换为布尔值true和false，
    其他值则作为字符串返回
    ```

--- apps/app-server/src/common/decorators/empty-to-undefined.decorator.ts
+++ apps/app-server/src/common/decorators/empty-to-undefined.decorator.ts
@@ -2,8 +2,16 @@ import { Transform } from 'class-transformer';
 
 export function EmptyToUndefined() {
     return Transform(({ value }) => {
-        if (value === '' || value === null || value === undefined)
+        if (value === '' || value === null || value === undefined) {
             return undefined;
-        return value as unknown;
+        }
+
+        switch (value) {
+            case 'true':
+                return true;
+            case 'false':
+                return false;
+        }
+        return value as string;
     });
 }
